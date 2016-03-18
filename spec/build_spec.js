27ed89e0b4ae0ff2d19e7ca012d861db24646083
Rune Madsen <rune@runemadsen.com>
2016-03-18 14:23:57 -0400

    layouts working

--- spec/build_spec.js
+++ spec/build_spec.js
@@ -1,21 +1,77 @@
 var build = require('../src/build.js');
 var fs = require('fs');
 
-describe("Build", function() {
+function buildContent(path) {
+  return fs.readFileSync("spec/support/book1/build/" + path);
+}
 
-  beforeAll(function(done) {
-    build({
-      files: ["spec/support/book1/content/*.md", "spec/support/book1/content/*.markdown"],
-      destination: "spec/support/book1/build/:format",
-      success: done
-    });
-  });
+describe("Build", function() {
 
   describe("Markdown", function() {
 
-    it("should convert markdown files", function() {
-      expect(fs.readFileSync("spec/support/book1/build/html/first-chapter.html")).toMatch("Heading</h1>")
-      expect(fs.readFileSync("spec/support/book1/build/html/second-chapter.html")).toMatch("Heading</h1>")
+    it("should convert markdown files", function(done) {
+      build({
+        files: "spec/support/book1/content/*.md",
+        destination: "spec/support/book1/build/:format",
+        success: function() {
+          expect(buildContent("html/first-chapter.html")).toMatch("Heading</h1>");
+          expect(buildContent("html/second-chapter.html")).toMatch("Heading</h1>");
+          done();
+        }
+      });
+    });
+
+  });
+
+  describe("Layout", function() {
+
+    it("should ignore layout", function(done) {
+      build({
+        files: "spec/support/book1/content/*.md",
+        destination: "spec/support/book1/build/:format",
+        success: function() {
+          expect(buildContent("html/first-chapter.html")).not.toMatch("Main layout");
+          expect(buildContent("html/first-chapter.html")).toMatch("Heading</h1>");
+          expect(buildContent("html/second-chapter.html")).not.toMatch("Main layout");
+          expect(buildContent("html/second-chapter.html")).toMatch("Heading</h1>");
+          done();
+        }
+      });
+    });
+
+    it("should use main layout", function(done) {
+      build({
+        layout: "spec/support/book1/layouts/main.html",
+        files: "spec/support/book1/content/*.md",
+        destination: "spec/support/book1/build/:format",
+        success: function() {
+          expect(buildContent("html/first-chapter.html")).toMatch("Main layout");
+          expect(buildContent("html/first-chapter.html")).toMatch("Heading</h1>");
+          expect(buildContent("html/second-chapter.html")).toMatch("Main layout");
+          expect(buildContent("html/second-chapter.html")).toMatch("Heading</h1>");
+          done();
+        }
+      });
+    });
+
+    it("should prioritize format layout", function(done) {
+      build({
+        layout: "spec/support/book1/layouts/main.html",
+        files: "spec/support/book1/content/*.md",
+        destination: "spec/support/book1/build/:format",
+        formats: {
+          html : {
+            layout: "spec/support/book1/layouts/format.html"
+          }
+        },
+        success: function() {
+          expect(buildContent("html/first-chapter.html")).toMatch("Format layout");
+          expect(buildContent("html/first-chapter.html")).toMatch("Heading</h1>");
+          expect(buildContent("html/second-chapter.html")).toMatch("Format layout");
+          expect(buildContent("html/second-chapter.html")).toMatch("Heading</h1>");
+          done();
+        }
+      });
     });
 
   });
--- src/build.js
+++ src/build.js
@@ -1,18 +1,26 @@
 var _ = require('lodash');
+var fs = require('fs');
 var vfs = require('vinyl-fs');
 var through = require('through2');
 var marked = require('marked');
 var gutil = require('gulp-util');
 var rimraf = require('rimraf');
+var tinyliquid = require('tinyliquid');
 
+// Get build destination for a single format
+// Returns: string
 function destination(config, format) {
   return config.destination.replace(":format", format);
 }
 
+// Check whether a vinyl file is a markdown file
+// Returns: boolean
 function isMarkdown(file) {
   return file.path.match(/\.md$/) || file.path.match(/\.markdown$/);
 }
 
+// through2 function to convert a markdown file to html
+// Returns: Vinyl filestream
 function markdown() {
   return through.obj(function (file, enc, cb) {
     if(isMarkdown(file)) {
@@ -31,12 +39,63 @@ function markdown() {
 	});
 }
 
+// Duplicates a stream. Used when the format chains diverge.
+// Returns: Duplicated file stream
+function duplicate() {
+  return through.obj(function(file, enc, cb) {
+    cb(null, file.clone());
+  });
+}
+
+// Applies the tinyliquid template layout to the file
+function assignLayout(file, layout, cb) {
+  var context = tinyliquid.newContext({ locals: { content: file.contents.toString() }});
+  layout(context, function(err) {
+    file.contents = new Buffer(context.getBuffer());
+    cb(null, file);
+  });
+}
+
+// Assigns layouts to the files in the stream.
+// Prioritizes format layout over main layout.
+var layoutCache = {};
+function layouts(config, format) {
+  var layout = _.get(config, "formats." + format + ".layout") || config.layout;
+  return through.obj(function(file, enc, cb) {
+    if(layout) {
+      if(layoutCache[layout]) {
+        assignLayout(file, layoutCache[layout], cb);
+      } else {
+        fs.readFile(layout, function (err, data) {
+          if (err) { return console.log(err); }
+          layoutCache[layout] = tinyliquid.compile(data.toString());
+          assignLayout(file, layoutCache[layout], cb);
+        });
+      }
+    } else {
+      cb(null, file);
+    }
+  });
+}
+
+// Main build function
 module.exports = function(config) {
+
+  // delete the build folders
   var folderGlob = destination(config, "") + "+(html|pdf|mobi|epub)"
   rimraf(folderGlob, function() {
-    vfs.src(config.files)
-      .pipe(markdown())
+
+    // preprocessing that needs to happen for all formats
+    var stream = vfs.src(config.files)
+      .pipe(markdown());
+
+    // html
+    var htmlStream = stream
+      .pipe(duplicate())
+      .pipe(layouts(config, "html"))
       .pipe(vfs.dest(destination(config, "html")))
-      .on('finish', config.success);
+      .on('finish', function() {
+        config.success("html")
+      });
   });
 }
