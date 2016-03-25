ba00c864d22e55bb9c30f5deae2546f3eb3714f7
Rune Madsen <rune@runemadsen.com>
2016-03-25 10:07:36 -0400

    merging into a nice config object

--- src/plugins/katex.js
+++ src/plugins/katex.js
@@ -1,4 +1,5 @@
 var through = require('through2');
+var path = require('path');
 var _ = require('lodash');
 
 module.exports = {
@@ -12,7 +13,9 @@ module.exports = {
 
       // add the katex CSS to all formats. This probably needs to
       // change as we test how katex performs in e-readers.
-      var stylesheets = _.get(payload, "config.formats." + format + ".stylesheets.files") || _.get(payload, "config.stylesheets.files");
+      payload.config.stylesheets = payload.config.stylesheets || {};
+      payload.config.stylesheets.files = payload.config.stylesheets.files || [];
+      payload.config.stylesheets.files.push(path.join(__dirname, "assets", "katex", "katex.css"));
 
       // no matter what, all hook functions must return
       // a through2 object, as it's a part of the stream chain.
