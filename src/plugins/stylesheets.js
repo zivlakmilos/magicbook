f016bd2cb6d1f7a950b6dbea9035e25c5f551c2d
Rune Madsen <rune@runemadsen.com>
2016-03-24 22:02:51 -0400

    cleanup

--- src/plugins/stylesheets.js
+++ src/plugins/stylesheets.js
@@ -2,28 +2,11 @@ var through = require('through2');
 
 module.exports = {
 
-  // all of them gets passed the format, so it
-  // knows whether to do something or not.
-
   hooks: {
 
     init: function(format, payload) {
-
-      // no matter what, all hook functions must return
-      // a through2 object, as it's a part of the chain.
-      return through.obj(function (file, enc, cb) {
-        cb(null, file);
-    	});
+      return through.obj();
     }
-
-    // preMarkdown
-
-    // postMarkdown
-
-    // a way to add extra output files in some formats, like mathjax support
-    // in the web versions. This will also require the ability to add extra
-    // file to the scss bundle
-
 
   }
 
