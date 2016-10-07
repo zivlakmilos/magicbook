fa861e43d0888deeb5c381b16df8b84f3870ca81
Rune Madsen <rune@runemadsen.com>
2016-10-07 12:30:55 -0400

    tweak

--- src/plugins/load.js
+++ src/plugins/load.js
@@ -90,6 +90,7 @@ function treeToStreams(parent, streams) {
 //       }
 //     ],
 //     vars : {
+//       label: "Part",
 //       myVariable: "Something"
 //     }
 //  }
@@ -105,14 +106,14 @@ function filesToTree(part) {
       // If there is no objects in the array, or the latest object
       // is a part, create a non-part object to hold the files.
       if(fileObjects.length == 0 || _.last(fileObjects).label) {
-        var vars = _.omit(part, ['label', 'files', 'parent', 'vars']);
+        var vars = _.omit(part, ['files', 'parent', 'vars']);
         fileObjects.push({ files: [], parent:part, vars: vars });
       }
 
       _.last(fileObjects).files.push(file);
     }
     else if(file.label && file.files) {
-      var vars = _.omit(file, ['label', 'files', 'parent', 'vars']);
+      var vars = _.omit(file, ['files', 'parent', 'vars']);
       var child = filesToTree({ label: file.label, files: file.files, parent:part, vars: vars});
       fileObjects.push(child);
     }
