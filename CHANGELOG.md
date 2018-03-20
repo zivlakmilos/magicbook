d5919b3bc7a404aac35bb2afa70bbd1ad6fff90a
Rune Madsen <rune@runemadsen.com>
2018-03-20 10:41:04 +0800

    node sass updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,23 +1,28 @@
 # Changelog
 
+## 0.1.16
+
+* Updating `node-sass` to work on newest OSX
+* Updating `prince` package
+
 ## 0.1.15
 
-- Updating cheerio to rc2
-- Removing unnecessary dependencies
+* Updating cheerio to rc2
+* Removing unnecessary dependencies
 
 ## 0.1.14
 
-- Fixing bug where cross references weren't working from files in subfolders
-- Fixing bug where cross references weren't working with permalinks
+* Fixing bug where cross references weren't working from files in subfolders
+* Fixing bug where cross references weren't working with permalinks
 
 ## 0.1.13
 
-- Bumping version of prince package
+* Bumping version of prince package
 
 ## 0.1.12
 
-- Speeding up liquid rendering by not adding liquid locals to each file where it's not needed.
+* Speeding up liquid rendering by not adding liquid locals to each file where it's not needed.
 
 ## 0.1.11
 
-- Fixed table of contents generation so all links are relative to the file the toc is inserted into.
+* Fixed table of contents generation so all links are relative to the file the toc is inserted into.
--- src/plugin_executer.js
+++ src/plugin_executer.js
@@ -4,10 +4,9 @@ var path = require('path');
 
 var Registry = function() {
   this.order = [];
-}
+};
 
 Registry.prototype = {
-
   add: function(label, fn) {
     this.order.push({
       name: label,
@@ -16,8 +15,18 @@ Registry.prototype = {
   },
 
   before: function(beforeLabel, label, fn) {
-    var beforeIndex = _.findIndex(this.order, function(o) { return o.name == beforeLabel; });
-    if (beforeIndex === -1) { return console.log("Cannot register", label, ".", beforeLabel, 'not found') }
+    var beforeIndex = _.findIndex(this.order, function(o) {
+      return o.name == beforeLabel;
+    });
+    if (beforeIndex === -1) {
+      return console.log(
+        'Cannot register',
+        label,
+        '.',
+        beforeLabel,
+        'not found'
+      );
+    }
     this.order.splice(beforeIndex, 0, {
       name: label,
       fn: fn
@@ -25,25 +34,32 @@ Registry.prototype = {
   },
 
   after: function(afterLabel, label, fn) {
-    var afterIndex = _.findIndex(this.order, function(o) { return o.name == afterLabel; });
-    if (afterIndex === -1) { return console.log("Cannot register", label, ".", afterLabel, 'not found') }
+    var afterIndex = _.findIndex(this.order, function(o) {
+      return o.name == afterLabel;
+    });
+    if (afterIndex === -1) {
+      return console.log(
+        'Cannot register',
+        label,
+        '.',
+        afterLabel,
+        'not found'
+      );
+    }
     this.order.splice(afterIndex + 1, 0, {
       name: label,
       fn: fn
     });
   }
-
-}
+};
 
 var Executer = function(verbose) {
   this.plugins = {};
   this.verbose = verbose;
-}
+};
 
 Executer.prototype = {
-
   execute: function(plugins, disablePlugins, args, cb) {
-
     // require all plugins. Plugin functions are cached
     // across builds.
     this.requirePlugins(plugins);
@@ -54,11 +70,14 @@ Executer.prototype = {
 
     // loop through and instantiate each plugin, passing in
     // the registry object.
-    _.each(plugins, _.bind(function(plugin) {
-      if(this.plugins[plugin]) {
-        new this.plugins[plugin](registry);
-      }
-    }, this));
+    _.each(
+      plugins,
+      _.bind(function(plugin) {
+        if (this.plugins[plugin]) {
+          new this.plugins[plugin](registry);
+        }
+      }, this)
+    );
 
     // remove disabled plugins by removing all labels that start
     // with the plugin name.
@@ -67,15 +86,17 @@ Executer.prototype = {
     });
 
     // log the order of all plugins.
-    if(this.verbose) {
-      console.log('Plugin order:')
+    if (this.verbose) {
+      console.log('Plugin order:');
       _.each(enabled, function(o, i) {
-        console.log((i+1) + '. ' + o.name);
-      })
+        console.log(i + 1 + '. ' + o.name);
+      });
     }
 
     // create an async waterfall chain from the registry order.
-    var chain = _.map(enabled, function(o) { return o.fn; });
+    var chain = _.map(enabled, function(o) {
+      return o.fn;
+    });
 
     // add a function to kick off the chain, passing the args
     chain.unshift(function(callback) {
@@ -93,46 +114,64 @@ Executer.prototype = {
   // ------------------------------------------------
 
   requirePlugins: function(plugins) {
-    _.each(plugins, _.bind(function(plugin) {
-      if(!this.plugins[plugin]) {
-        this.plugins[plugin] = this.requirePlugin(plugin);
-      }
-    }, this));
+    _.each(
+      plugins,
+      _.bind(function(plugin) {
+        if (!this.plugins[plugin]) {
+          this.plugins[plugin] = this.requirePlugin(plugin);
+        }
+      }, this)
+    );
   },
 
   requirePlugin: function(file) {
-
     var loadedFile;
 
     // try to load the file as a local file
-    try { loadedFile = require(path.join(__dirname, 'plugins', file)); }
-      catch (e1) {
-        if(e1 instanceof SyntaxError) {
-          console.log("Plugin file: " + file + " has syntax errors. " + e1.toString());
-        } else {
-          // try to load the file as a file in the book
-          try { loadedFile = require(path.join(process.cwd(), file)); } catch(e2) {
-            if(e2 instanceof SyntaxError) {
-              console.log("Plugin file: " + file + " has syntax errors. " + e2.toString());
-            } else {
-              // try to load the file as a node module installed in book folder
-              try { loadedFile = require(path.join(process.cwd(), 'node_modules', file)); } catch(e3) {
-                console.log(e3)
-                if(e3 instanceof SyntaxError) {
-                  console.log("Plugin file: " + file + " has syntax errors. " + e3.toString());
-                } else {
-                  console.log("Required file: " + file + " cannot be found");
-                }
+    try {
+      loadedFile = require(path.join(__dirname, 'plugins', file));
+    } catch (e1) {
+      if (e1 instanceof SyntaxError) {
+        console.log(
+          'Plugin file: ' + file + ' has syntax errors. ' + e1.toString()
+        );
+      } else {
+        // try to load the file as a file in the book
+        try {
+          loadedFile = require(path.join(process.cwd(), file));
+        } catch (e2) {
+          if (e2 instanceof SyntaxError) {
+            console.log(
+              'Plugin file: ' + file + ' has syntax errors. ' + e2.toString()
+            );
+          } else {
+            // try to load the file as a node module installed in book folder
+            try {
+              loadedFile = require(path.join(
+                process.cwd(),
+                'node_modules',
+                file
+              ));
+            } catch (e3) {
+              console.log(e3);
+              if (e3 instanceof SyntaxError) {
+                console.log(
+                  'Plugin file: ' +
+                    file +
+                    ' has syntax errors. ' +
+                    e3.toString()
+                );
+              } else {
+                console.log('Required file: ' + file + ' cannot be found');
               }
             }
           }
         }
-
+      }
     }
 
     return loadedFile;
   }
-
-}
+};
 
 module.exports = Executer;
