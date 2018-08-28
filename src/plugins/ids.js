f3f06666c0d026cdb013196bc505f612b1efa40c
Daniel Shiffman <daniel.shiffman@gmail.com>
2018-08-27 20:34:50 -0400

    removing stray whitespace

--- src/plugins/ids.js
+++ src/plugins/ids.js
@@ -56,7 +56,7 @@ Plugin.prototype = {
           }
         });
 
-        file.contents = new Buffer(file.$el.root().html());    
+        file.contents = new Buffer(file.$el.root().html());
 
         cb(null, file);
       })
